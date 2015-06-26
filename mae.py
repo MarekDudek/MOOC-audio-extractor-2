4f86f37b66131461368a6155b9a8e75696fcd64a
Marek Dudek <marek.dudek@gmail.com>
2015-06-26 21:58:13 +0200

    Basic command line arguments processing

--- mae.py
+++ mae.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 
+import sys
+import os
+
 if __name__ == '__main__':
+
     print 'MOOC audio extractor'
+
+    if len(sys.argv) != (2 + 1):
+        print 'Two arguments required, input and output directories'
+        sys.exit(1)
+
+    input_dir  = sys.argv[1]
+    output_dir = sys.argv[2]
+
+    print 'Input  directory is "%s"' % (input_dir, )
+    print 'Output directory is "%s"' % (output_dir, )
