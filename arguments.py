81c98e1eca4c91373d624a69e529e704d30ecaa1
Marek Dudek <marek.dudek@gmail.com>
2015-06-27 12:58:58 +0200

    got rid off sys from arguments validation

--- arguments.py
+++ arguments.py
@@ -1,25 +1,23 @@
-import sys
 import os
 
-def validate_arguments():
+def validate_arguments(args, on_fail):
 
-    args = sys.argv[1:]
     if len(args) != 2:
         print 'Two arguments required, input and output directories'
-        sys.exit(1)
+        on_fail()
 
-    input_dir  = sys.argv[1]
-    output_dir = sys.argv[2]
+    input_dir  = args[0]
+    output_dir = args[1]
 
     print 'Input  directory is "{0}"'.format(input_dir)
     print 'Output directory is "{0}"'.format(output_dir)
 
     if not os.path.isdir(input_dir):
         print 'Input directory does not exist and it should'
-        sys.exit(1)
+        on_fail()
 
     if os.path.isdir(output_dir):
         print 'Output directory exists and it should not'
-        sys.exit(1)
+        on_fail()
 
     return input_dir, output_dir
--- mae.py
+++ mae.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 
 import sys
+
 sys.path.append('.')
 from arguments  import *
 from hierarchy_processing import *
+
+import functools
 
 
 if __name__ == '__main__':
 
     print 'MOOC audio extractor'
-    input_dir, output_dir = validate_arguments()
+
+    arguments = sys.argv[1:]
+    on_fail   = functools.partial(sys.exit, 1)
+    input_dir, output_dir = validate_arguments(arguments, on_fail)
+
     process_directory(input_dir, output_dir)
