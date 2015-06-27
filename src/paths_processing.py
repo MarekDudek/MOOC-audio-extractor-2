b76beba9cab98154c84fdae174af573ed17f7229
Marek Dudek <marek.dudek@gmail.com>
2015-06-27 18:58:29 +0200

    Extracting lecture order and title from lecture input directory

--- src/paths_processing.py
+++ src/paths_processing.py
@@ -5,11 +5,37 @@ def lecture_subdirectory(dirpath, output_dir):
     subdirectory = os.path.join(output_dir, base_name)
     return subdirectory
 
+def lecture_subdirectory2(dirpath, output_dir):
+    subdir = lecture_input_subdirectory(dirpath)
+    friendly_name = lecture_sortable_name(subdir)
+    pass
+
+def lecture_input_subdirectory(dirpath):
+    return os.path.basename(dirpath)
+
+def lecture_sortable_name(lecture_input_subdir):
+    pass
+
+
+import re
+LECTURE_INPUT_SUBDIRECTORY = re.compile(r'^ *\w+ *(\d+) *: *(.+?) *$')
+
+def extract_lecture_order_and_title(lecture_input_subdir):
+    match = LECTURE_INPUT_SUBDIRECTORY.match(lecture_input_subdir)
+    if match:
+        order = match.group(1)
+        title = match.group(2)
+        return int(order), title
+
 def rebuild_input_file_path(dirpath, name):
     return os.path.join(dirpath, name)
+
+
 
 def output_file_path(input_dir, output_dir, dirpath, name) :
     input_file = rebuild_input_file_path(dirpath, name)
     relative_path = os.path.relpath(input_file, input_dir)
     (root, ext) = os.path.splitext(relative_path)
     return os.path.join(output_dir, root + '.mp3')
+
+
--- test/paths_processing_test.py
+++ test/paths_processing_test.py
@@ -15,6 +15,54 @@ class PathsProcessing(unittest.TestCase):
         # then
         self.assertEquals(subdirectory_path, '/home/marek/Devel/varia/emb3/Lec 10: Dealers and Liquid Security Markets')
 
+    def test_finding_lecture_input_subdirectory(self):
+        # given
+        dirpath    = '/home/marek/Education/finance/Economics of Money and Banking, Part One/Lec 10: Dealers and Liquid Security Markets'
+        # when
+        lecture_input_subdir = lecture_input_subdirectory(dirpath)
+        # then
+        self.assertEquals(lecture_input_subdir, 'Lec 10: Dealers and Liquid Security Markets')
+    
+    def itest_lecture_friendly_name_two_digits(self):
+        # given
+        lecture_input_subdir = 'Lec 10: Dealers and Liquid Security Markets'
+        # when
+        friendly_name = lecture_sortable_name(lecture_input_subdir)
+        # then
+        self.assertEquals(friendly_name, '10 Dealers and Liquid Security Markets')
+
+    def itest_lecture_friendly_name_one_digits(self):
+        # given
+        lecture_input_subdir = 'Lec 3: Money and the State: Domestic'
+        # when
+        friendly_name = lecture_sortable_name(lecture_input_subdir)
+        # then
+        self.assertEquals(friendly_name, '03 Money and the State: Domestic')
+
+    def test_lecture_title(self):
+        # given
+        lectures = [
+            'Lec 1: The Four Prices of Money',
+            'Lec 2:    The Natural Hierarchy of Money',
+            'Lec 3: Money and the State: Domestic   ',
+            'Lec 4   : The Money View, Micro and Macro',
+            'Lec    5: The Central Bank as a Clearinghouse',
+            '   Lec 6: Federal Funds, Final Settlement',
+            'Lec 7: Repos, Postponing Settlement',
+            'Lec 8: Eurodollars, Parallel Settlement',
+            'Lec 9: The World that Bagehot Knew',
+            'Lec 10: Dealers and Liquid Security Markets',
+            'Lec 11: Banks and the Market for Liquidity',
+            'Lec 12: Lender-Dealer of Last Resort'
+        ]
+
+        # when
+        orders_and_titles = map(extract_lecture_order_and_title, lectures)
+
+        # then
+        self.assertEquals(len(orders_and_titles), len(lectures))
+    
+
     def test_rebuild_input_file_path(self):
         # given
         dirpath = '/home/marek/Education/finance/Economics of Money and Banking, Part One/Lec 10: Dealers and Liquid Security Markets'
@@ -34,6 +82,7 @@ class PathsProcessing(unittest.TestCase):
         output_file = output_file_path(input_dir, output_dir, dirpath, name)
         # then
         self.assertEquals(output_file, '/home/marek/Devel/varia/emb3/Lec 10: Dealers and Liquid Security Markets/11 - 1 - 10-1 FT-  Asymmetric Credit Growth in Europe (6-35).mp3')
+
 
 if __name__ == '__main__':
     unittest.main()
