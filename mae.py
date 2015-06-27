0d7b77074dae25e7abd37eef573b59f186bd2240
Marek Dudek <marek.dudek@gmail.com>
2015-06-27 12:49:24 +0200

    processing of directory hierarchy in separete module

--- mae.py
+++ mae.py
@@ -1,31 +1,10 @@
 #!/usr/bin/env python
 
 import sys
-import os
-
 sys.path.append('.')
-from extraction import *
 from arguments  import *
+from hierarchy_processing import *
 
-def process_directory(input_dir, output_dir):
-
-    for dirpath, dirnames, filenames in os.walk(input_dir):
-        print 'Directory "{0}"'.format(dirpath)
-        if dirpath == input_dir: 
-            os.makedirs(output_dir)
-        else:
-            base_path = os.path.basename(dirpath)
-            output_subdir = os.path.join(output_dir, base_path)
-            os.makedirs(output_subdir)
-
-        for name in filenames:
-            input_file = os.path.join(dirpath, name)
-            print '\tInput  file "{0}"'.format(input_file)
-            relative_path = os.path.relpath(input_file, input_dir)
-            (root, ext) = os.path.splitext(relative_path)
-            output_file = os.path.join(output_dir, root + '.mp3')
-            print '\tOutput file "{0}"'.format(output_file)
-            extract_audio(input_file, output_file)
 
 if __name__ == '__main__':
 
